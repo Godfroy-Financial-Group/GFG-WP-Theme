c83d9d56a194ec98d332c71f0546a43c0eb82618
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-08 12:33:26 -0400

    Updated Content

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 <div class="row">
     <!-- Content -->
     <div class="col-8">
-
+        <?php 
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+				get_template_part( 'content', get_post_format() );
+			endwhile; endif; 
+        ?>
     </div>
+
     <!-- Sidebar -->
     <?php get_sidebar(); ?>
 </div>
