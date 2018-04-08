95c594a42e4846772fe09d3a8dd51c574772c9bc
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-08 13:04:22 -0400

    Comments and Single Page View

--- index.php
+++ index.php
@@ -4,10 +4,18 @@
     <!-- Content -->
     <div class="col-8">
         <?php 
-			if ( have_posts() ) : while ( have_posts() ) : the_post();
-				get_template_part( 'content-blog', get_post_format() );
-			endwhile; endif; 
-        ?>
+            if ( have_posts() ) : 
+                while ( have_posts() ) : the_post();
+				    get_template_part( 'content', get_post_format() );
+                endwhile;  ?>
+
+                <nav>
+                    <ul class="pager">
+                        <li><?php next_posts_link( 'Previous' ); ?></li>
+                        <li><?php previous_posts_link( 'Next' ); ?></li>
+                    </ul>
+                </nav>        
+            <?php endif; ?>
     </div>
 
     <!-- Sidebar -->
