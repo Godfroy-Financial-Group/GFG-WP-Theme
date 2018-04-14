8c268c33711b6899d3d0fa4c44e878499b933ec1
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-14 12:24:31 -0400

    Added a slight delay to the submenu toggles
    To help with hover edgecases

--- libs/bootstrap-4-multi-dropdown-navbar/js/bootstrap-4-navbar.js
+++ libs/bootstrap-4-multi-dropdown-navbar/js/bootstrap-4-navbar.js
@@ -8,25 +8,27 @@
 
 $( document ).ready( function () {
     function preformSubmenuDropdownToggle(e) {
-        var $el = $( this );
-        var $parent = $( this ).offsetParent( ".dropdown-menu" );
-        if ( !$( this ).next().hasClass( 'show' ) ) {
-            $( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
-        }
-        var $subMenu = $( this ).next( ".dropdown-menu" );
-        $subMenu.toggleClass( 'show' );
-        
-        $( this ).parent( "li" ).toggleClass( 'show' );
-
-        $( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
-            $( '.dropdown-menu .show' ).removeClass( "show" );
-        } );
-        
-         if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
-            $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
-        }
-
-        return false;
+        setTimeout(function(e) {
+            var $el = $( this );
+            var $parent = $( this ).offsetParent( ".dropdown-menu" );
+            if ( !$( this ).next().hasClass( 'show' ) ) {
+                $( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
+            }
+            var $subMenu = $( this ).next( ".dropdown-menu" );
+            $subMenu.toggleClass( 'show' );
+            
+            $( this ).parent( "li" ).toggleClass( 'show' );
+    
+            $( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
+                $( '.dropdown-menu .show' ).removeClass( "show" );
+            } );
+            
+             if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
+                $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
+            }
+    
+            return false;
+        }, 500, e);
     }
     $( '.dropdown-menu a.dropdown-toggle' ).on( 'click', preformSubmenuDropdownToggle);
     
