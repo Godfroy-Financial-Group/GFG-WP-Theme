1a3e76caf333b659aa6e07068d00e9b08fe01914
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-14 23:45:40 -0400

    Updating Header Styles

--- js/universal.js
+++ js/universal.js
@@ -1,11 +1,11 @@
-$('body').on('mouseenter mouseleave','.dropdown',function(e){
-  var _d=$(e.target).closest('.dropdown');_d.addClass('show');
-  setTimeout(function(){
-    _d[_d.is(':hover')?'addClass':'removeClass']('show');
-  },300);
-});
-
 $(document).ready(function() {
+  $('body').on('mouseenter mouseleave','.dropdown',function(e){
+    var _d=$(e.target).closest('.dropdown');_d.addClass('show');
+    setTimeout(function(){
+      _d[_d.is(':hover')?'addClass':'removeClass']('show');
+    },300);
+  });
+
   new ResizeSensor(jQuery('.navbar-primary'), function() {
     $(".page-title-bar").css("margin-top", $(".navbar-primary").outerHeight());
   });
