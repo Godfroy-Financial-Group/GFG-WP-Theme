d780a863df0daec463f7c7736f19bfcb7b39c5e1
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-14 23:58:15 -0400

    Mess with JS

--- js/universal.js
+++ js/universal.js
@@ -1,12 +1,16 @@
-$(document).ready(function() {
-  $('body').on('mouseenter mouseleave','.dropdown',function(e){
-    var _d=$(e.target).closest('.dropdown');_d.addClass('show');
-    setTimeout(function(){
-      _d[_d.is(':hover')?'addClass':'removeClass']('show');
-    },300);
-  });
+// Pre Document Ready
+$('body').on('mouseenter mouseleave','.dropdown',function(e){
+  var _d=$(e.target).closest('.dropdown');_d.addClass('show');
+  setTimeout(function(){
+    _d[_d.is(':hover')?'addClass':'removeClass']('show');
+  },300);
+});
 
-  new ResizeSensor(jQuery('.navbar-primary'), function() {
-    $(".page-title-bar").css("margin-top", $(".navbar-primary").outerHeight());
-  });
+new ResizeSensor(jQuery('.navbar-primary'), function() {
+  $(".page-title-bar").css("margin-top", $(".navbar-primary").outerHeight());
+});
+
+// Post Document Ready
+$(document).ready(function() {
+
 });
