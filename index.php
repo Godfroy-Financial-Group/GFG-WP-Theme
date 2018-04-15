4d4da3378c79adb88d092899e996cc0cb6d6ac31
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-15 01:30:48 -0400

    g

--- index.php
+++ index.php
@@ -16,10 +16,10 @@
                     <nav>
                         <ul class="pager list-inline">
                             <?php if ($previousPostLink): ?>
-                                <li class="list-inline-item btn btn-secondary"><?php echo $previousPostLink; ?></li>
+                                <li class="list-inline-item btn btn-secondary">Previous</li>
                             <?php endif; ?>
                             <?php if ($nextPostLink): ?>
-                                <li class="list-inline-item btn btn-secondary"><?php echo $nextPostLink; ?></li>
+                                <li class="list-inline-item btn btn-secondary">Next</li>
                             <?php endif; ?> 
                         </ul>
                     </nav>     
