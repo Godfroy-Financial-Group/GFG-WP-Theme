d59335696c6575d65bc48b975fd6621e3fe77ecb
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-15 01:25:24 -0400

    Finetuning NExt/Previous

--- index.php
+++ index.php
@@ -4,17 +4,27 @@
     <!-- Content -->
     <div class="col-8">
         <?php 
-            if ( have_posts() ) : 
-                while ( have_posts() ) : the_post();
+            if (have_posts()): 
+                while (have_posts()) {  
+                    the_post();
 				    get_template_part( 'content', get_post_format() );
-                endwhile;  ?>
+                }
 
-                <nav>
-                    <ul class="pager list-inline">
-                        <li class="list-inline-item btn btn-secondary"><?php previous_posts_link('« Previous'); ?></li>
-                        <li class="list-inline-item btn btn-secondary"><?php next_posts_link('Next »'); ?></li>
-                    </ul>
-                </nav>        
+                $previousPostLink = previous_posts_link('« Previous');
+                $nextPostLink = next_posts_link('Next »');
+
+                if ($previousPostLink || $nextPostLink): ?>
+                    <nav>
+                        <ul class="pager list-inline">
+                            <?php if ($previousPostLink): ?>
+                                <li class="list-inline-item btn btn-secondary"><?php previous_posts_link('« Previous'); ?></li>
+                            <?php endif; ?>
+                            <?php if ($nextPostLink): ?>
+                                <li class="list-inline-item btn btn-secondary"><?php  ?></li>
+                            <?php endif; ?>
+                        </ul>
+                    </nav>     
+                <?php endif; ?>
             <?php endif; ?>
     </div>
 
