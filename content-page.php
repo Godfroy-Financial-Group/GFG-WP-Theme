28844b1e6b6d5565f7b9883ba3bceb7c05a1e87f
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-04-14 22:50:20 -0400

    Added new CSS

--- content-page.php
+++ content-page.php
@@ -1,6 +1,6 @@
 <div class="blog-post">
-	<div class="row page-title">
-		<h1><?php the_title(); ?></h1>
+	<div class="page-title clearfix">
+		<h1 class="float-right"><?php the_title(); ?></h1>
 	</div>
 	<div class="row">
 		<p><?php the_content(); ?></p>
--- header.php
+++ header.php
@@ -16,6 +16,7 @@
     <!-- And now our site stylesheets -->
     <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/universal.css">
     <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/home.css">
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/blog.css">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
